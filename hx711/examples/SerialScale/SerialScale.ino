00384302ec8ae825678796a504dfb74d8397a79a
baruc <baruc.almaguer@gmail.com>
2017-09-07 19:05:26 -0500

    Modificado ejemplo de serialScale, incluidos parametros de calibracion de nuestra bascula

--- hx711/examples/SerialScale/SerialScale.ino
+++ hx711/examples/SerialScale/SerialScale.ino
@@ -11,11 +11,21 @@ Hx711 scale(A1, A0);
 void setup() {
 
   Serial.begin(9600);
+  Serial.println("Scale test");
+  
+  //Calibracion:
+  scale.setReadTimes(32);
+  //long val = scale.averageValue();
+  scale.setScale(0.0050922f);
+  scale.setOffset(-42449.76f);
+  //x=8337202.0 y = 0gramos
+  //x=8533800.0 y = 1001gramos
 
+  //Fin calibracion
+  scale.setReadTimes(4);
 }
 
 void loop() {
-
   Serial.print(scale.getGram(), 1);
   Serial.println(" g");
 
